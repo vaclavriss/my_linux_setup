300724da1ee8e6eedda9033c1763b2d775d88cb8
Vaclav <vaclav.riss@gmail.com>
2026-01-19 17:24:00 +0100

    modified install

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # install pet
 cp pet/bin/pet /usr/local/bin/.
 
+
+
 rm -r ~/.config/pet
 cp -r pet/config/pet ~/.config/.
+
+echo 'alias pc='pet searo "alias pc='pet search | xclip -selection clipboard && xdotool key Ctrl+Shift+V'" >> ~/.bashrc
 
