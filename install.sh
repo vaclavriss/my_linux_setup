26922c120e5e09189e7598bccd53f138e2925429
Vaclav Riss <vaclav.riss@gmail.com>
2026-04-11 21:18:51 +0200

    updatre

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
+# install dependecies
+sudo apt install fzf
+sudo apt install xclip
+sudo apt install xdotool
 # install pet
-cp pet/bin/pet /usr/local/bin/.
-
+sudo cp pet/bin/pet /usr/local/bin/.
 
 # remove old config
 rm -r ~/.config/pet
-cp -r pet/config/pet ~/.config/.
+mkdir ~/.config/pet
+cp  pet/config/config.toml  ~/.config/pet/.
+cp  pet/config/snippet.toml  ~/.config/pet/.
 
 echo 'alias pc='pet searo "alias pc='pet search | xclip -selection clipboard && xdotool key Ctrl+Shift+V'" >> ~/.bashrc
 
