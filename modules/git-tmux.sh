cdf9bb6816cb37b7d7d131d8aeb34b53dd15a431
Vaclav Riss <vaclav.riss@gmail.com>
2026-05-06 12:51:44 +0200

    fix

--- modules/git-tmux.sh
+++ modules/git-tmux.sh
@@ -2,5 +2,4 @@
 # Git / SSH / tmux
 
 sudo apt install -y openssh-client tmux || true
-copy_file_if_exists "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 copy_file_if_exists "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
